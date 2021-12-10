f7c12d1a2219e3a3e10117bbc5e24d4bafb239a2
Timo <66961498+nuokey@users.noreply.github.com>
2021-12-10 14:54:28 +0300

    Update

--- nuoclick/script.js
+++ nuoclick/script.js
@@ -1,12 +1,20 @@
 let clickButton = document.querySelector('.click');
 
 clickButton.onclick = function() {
-    clicks = document.getElementById("clicks-clicked")
-    clicks.innerHTML = Number(clicks.textContent)+1;
+    clicks = document.getElementById("clicks-clicked");
+    clicksPerClick = document.getElementById("clicks-per-click");
+    clicks.innerHTML = Number(clicks.textContent) + Number(clicksPerClick.textContent);
 }
 
 let buyButton = document.querySelector('.buy')
 
 buyButton.onclick = function() {
-    clicks = document.getElementById("clicks-clicked")
+    clicks = document.getElementById("clicks-clicked");
+    upgradeCost = document.getElementById("upgrade-cost");
+    clicksPerClick = document.getElementById("clicks-per-click");
+    if (Number(clicks.textContent) >= Number(upgradeCost.textContent)) {
+        clicks.innerHTML = Number(clicks.textContent) - Number(upgradeCost.textContent);
+        clicksPerClick.innerHTML = Number(clicksPerClick.textContent) + 1;
+        upgradeCost.innerHTML = Number(upgradeCost.textContent) * 2;
+    }
 }
