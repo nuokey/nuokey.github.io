802cf344930dcfc965c20062c7281415540f4bf7
Timo <66961498+nuokey@users.noreply.github.com>
2021-12-11 12:58:18 +0300

    Update script.js

--- nuoclick/script.js
+++ nuoclick/script.js
@@ -1,8 +1,11 @@
 document.cookie = "coins=123"
 let indexCoins = document.cookie.indexOf("coins", 0) + 6;
 let indexSimbol = document.cookie.indexOf(";", indexCoins);
+if (indexSimbol == -1) {
+    let coins = document.cookie[indexCoins, length(document.cookie) - 1];
+}
 let coins = document.cookie[indexCoins, indexSimbol];
-document.getElementById("coins").innerHTML = document.cookie;
+document.getElementById("coins").innerHTML = coins;
 
 let coinsPerClick = document.getElementById("coins-per-click");
 
