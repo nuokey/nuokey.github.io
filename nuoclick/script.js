f8e28aca0c1c27836f41ed9c48d95134ab4b792a
Timo <66961498+nuokey@users.noreply.github.com>
2021-12-11 11:45:13 +0300

    Update script.js

--- nuoclick/script.js
+++ nuoclick/script.js
@@ -1,21 +1,21 @@
-document.cookie = encodeURIComponent("user") + "=" + encodeURIComponent("John");
-document.getElementById("coins").innerHTML = document.cookie;
+document.cookie = "user=Timo";
+alert(document.cookie);
 
 let clickButton = document.querySelector('.click');
 
 clickButton.onclick = function() {
-    coins = document.getElementById("coins");
-    coinsPerClick = document.getElementById("coins-per-click");
-    finalCoins = Number(coins.textContent) + Number(coinsPerClick.textContent)
+    let coins = document.getElementById("coins");
+    let coinsPerClick = document.getElementById("coins-per-click");
+    let finalCoins = Number(coins.textContent) + Number(coinsPerClick.textContent)
     coins.innerHTML = finalCoins.toFixed(3);
 }
 
 let buyButton = document.querySelector('.buy')
 
 buyButton.onclick = function() {
-    coins = document.getElementById("coins");
-    upgradeCost = document.getElementById("upgrade-cost");
-    coinsPerClick = document.getElementById("coins-per-click");
+    let coins = document.getElementById("coins");
+    let upgradeCost = document.getElementById("upgrade-cost");
+    let coinsPerClick = document.getElementById("coins-per-click");
     if (Number(coins.textContent) >= Number(upgradeCost.textContent)) {
         coins.innerHTML = (Number(coins.textContent) - Number(upgradeCost.textContent)).toFixed(3);
         coinsPerClick.innerHTML = Number(coinsPerClick.textContent) + 0.001;
