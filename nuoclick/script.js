a7ff657cdb1b5c3d752e7d3f186e361b24975e38
Timo <66961498+nuokey@users.noreply.github.com>
2021-12-11 15:32:47 +0300

    Update script.js

--- nuoclick/script.js
+++ nuoclick/script.js
@@ -6,7 +6,7 @@ if (indexSimbol == -1) {
 else {
     let coins = document.cookie[indexCoins, indexSimbol];
 }
-document.getElementById("coins").innerHTML = document.cookie.slice(6, document.cookie.indexOf(";", 0));
+document.getElementById("coins").innerHTML = coins;
 
 let coinsPerClick = document.getElementById("coins-per-click");
 
