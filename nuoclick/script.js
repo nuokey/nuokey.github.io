dedb597b8146f1ef6bb066f7e33086da84323f54
Timo <66961498+nuokey@users.noreply.github.com>
2021-12-11 14:59:56 +0300

    Update

--- nuoclick/script.js
+++ nuoclick/script.js
@@ -18,9 +18,10 @@ clickButton.onclick = function() {
     let coinsPerClick = document.getElementById("coins-per-click");
     let finalCoins = Number(coins.textContent) + Number(coinsPerClick.textContent)
     coins.innerHTML = finalCoins.toFixed(3);
+    document.cookie = "coins=" + finalCoins.toFixed(3);
 }
 
-let buyButton = document.querySelector('.buy')
+let buyButton = document.querySelector('.buy');
 
 buyButton.onclick = function() {
     let coins = document.getElementById("coins");
@@ -30,5 +31,14 @@ buyButton.onclick = function() {
         coins.innerHTML = (Number(coins.textContent) - Number(upgradeCost.textContent)).toFixed(3);
         coinsPerClick.innerHTML = Number(coinsPerClick.textContent) + 0.001;
         upgradeCost.innerHTML = Number(upgradeCost.textContent) * 2;
+
+        document.cookie = "coins=" + ((Number(coins.textContent) - Number(upgradeCost.textContent)).toFixed(3)) + "; coins-per-click=" + (Number(coinsPerClick.textContent) + 0.001);
     }
+}
+
+let deleteButton = document.querySelector("delete");
+
+deleteButton.onclick = function() {
+    document.cookie = "coins=0; coins-per-click=0.001"
+    location.reload()
 }
