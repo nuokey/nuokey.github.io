7e49444aa4a0bdfff8d7a6ca31bf3ccc88514d3e
Timo <66961498+nuokey@users.noreply.github.com>
2021-12-20 13:48:28 +0300

    Update data_load.js

--- nuoclick/data_load.js
+++ nuoclick/data_load.js
@@ -5,6 +5,6 @@ if (indexSimbol == -1) {
     coinsLoaded = document.cookie.slice(indexCoins);
 }
 else {
-    coinsLoaded = document.cookie.slice(indexCoins, indexSimbol);
+    coinsLoaded = document.cookie.slice(indexCoins + 6, indexSimbol);
 }
 document.getElementById("coins").innerHTML = coinsLoaded;
