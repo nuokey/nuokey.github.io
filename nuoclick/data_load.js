b05f7cd719b8e4913255dc4460c55d05ee2cfb1e
Timo <66961498+nuokey@users.noreply.github.com>
2021-12-30 20:36:29 +0300

    Update data_load.js

--- nuoclick/data_load.js
+++ nuoclick/data_load.js
@@ -2,7 +2,7 @@ let indexCoins = document.cookie.indexOf("coins=", 0);
 let indexSimbol = document.cookie.indexOf(";", indexCoins);
 let coinsLoaded = "Coins haven't been loaded";
 if (indexSimbol == -1) {
-    coinsLoaded = document.cookie.slice(indexCoins);
+    coinsLoaded = document.cookie.slice(indexCoins + 6);
 }
 else {
     coinsLoaded = document.cookie.slice(indexCoins + 6, indexSimbol);
