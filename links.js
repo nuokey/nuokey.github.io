200fa4e76c23ebcdd2bbe67a8b9e92689d344097
Timo <66961498+nuokey@users.noreply.github.com>
2022-03-02 22:57:55 +0300

    ButtonsUpdate

---
+++ links.js
@@ -0,0 +1,12 @@
+downloadLatest = document.getElementById("download-latest");
+
+downloadLatest.onclick = function() {
+    window.location.href = "download/versions/Build 1902.zip";
+};
+
+downloadPage = document.getElementById("download-page");
+
+downloadPage.onclick = function() {
+    window.location.href = "download/"
+    alert("ahah")
+}
