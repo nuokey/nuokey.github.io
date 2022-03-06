e019994c48d6dec6242e1cde5c714f172c375609
Timo <66961498+nuokey@users.noreply.github.com>
2022-03-06 22:49:24 +0300

    Update

---
+++ headerLinks.js
@@ -0,0 +1,11 @@
+homePage = document.getElementById("home-page");
+
+homePage.onclick = function() {
+    window.location.href = "/home/";
+};
+
+downloadPage = document.getElementById("download-page");
+
+downloadPage.onclick = function() {
+    window.location.href = "/download/";
+}
--- home/links.js
+++ home/links.js
@@ -3,9 +3,3 @@ downloadLatest = document.getElementById("download-latest");
 downloadLatest.onclick = function() {
     window.location.href = "download/versions/Build 1902.zip";
 };
-
-downloadPage = document.getElementById("download-page");
-
-downloadPage.onclick = function() {
-    window.location.href = "download/"
-}
