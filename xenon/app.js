85441d6cde03f70c807a48bb553bb9ceef9fe480
Timo <66961498+nuokey@users.noreply.github.com>
2023-02-03 13:05:43 +0300

    Update

---
+++ xenon/app.js
@@ -0,0 +1,25 @@
+const http = require("http");
+const fs = require("fs");
+
+let fileContent = fs.readFileSync("test.txt", "utf8");
+  
+http.createServer(function(request, response){
+      
+    console.log(`Запрошенный адрес: ${request.url}`);
+    // получаем путь после слеша
+    const filePath = request.url.substr(1);
+    fs.readFile(filePath, function(error, data){
+              
+        if(error){
+                  
+            response.statusCode = 404;
+            response.end("Resourse not found!");
+        }   
+        else{
+            response.end(data);
+        }
+    });
+}).listen(3000, function(){
+    console.log("Server started at 3000");
+});
+
---
+++ xenon/script.js
@@ -0,0 +1,74 @@
+input = document.getElementById("file-open");
+
+textField = document.getElementById("text-field");
+
+let cursorCoord = 0;
+
+let lastText = "";
+
+
+function openFile(input) {
+  let file = input.files[0];
+
+  let htmlText = "";
+
+  let reader = new FileReader();
+
+  reader.readAsText(file);
+
+  reader.onload = function() {
+    let text = reader.result;
+    let text2 = "";
+
+    
+    // text = text.replace("Hello", "dfsdfsdf");
+    // text = text.replace(" ", "&nbsp");
+    // text = text.replace("    ", "&nbsp&nbsp&nbsp&nbsp");
+
+    for (let i = 0; i < text.length; i++) {
+      if (text[i] == " ") {
+        text2 += "&nbsp";
+      }
+      else {
+        text2 += text[i];
+      }
+    }
+
+    text2 = text2.split("\r\n");
+
+    console.log(text2);
+    
+    text2.forEach(element => {
+        htmlText += `<div>${element}</div>`;
+    });
+
+    // htmlText.replace(" ", "&nbsp");
+
+    textField.innerHTML = htmlText;
+  };
+}
+
+function saveFile() {
+    var data = textField.innerHTML;
+    let data2 = "";
+    // console.log(data);
+
+    while (data.replace("<div>", "") != data) {
+        data = data.replace("<div>", "");
+    }
+    while (data.replace("</div>", "\n") != data) {
+        data = data.replace("</div>", "\n");
+    }
+    while (data.replace("&nbsp;", " ") != data) {
+        data = data.replace("&nbsp;", " ");
+    }
+    console.log(data);
+
+    var a = document.getElementById("linkForSavingFiles");
+    var file = new Blob([data], {
+      type: 'plain/text'
+    });
+    a.href = URL.createObjectURL(file);
+    a.download = 'file.txt';
+    a.click();
+}
