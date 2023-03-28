5e15d56dbcbb10418395aae4710afa7438f39393
Timo <66961498+nuokey@users.noreply.github.com>
2023-03-28 22:52:59 +0300

    Update

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
-let textHTML = document.querySelector(".text");
-let inputHTML = document.querySelector("#number");
+playerImg = document.querySelector(".player")
+aiImg = document.querySelector(".ai")
+resultDiv = document.querySelector(".result")
 
-async function requestNumber() {
-    let input = inputHTML.value;
-    let responce = await fetch(`https://kruase.ml/`);
-    let text = await responce.text()
+async function requestCards() {
+    let responce = await fetch(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`);
 
-    console.log(text);
+    let text = await responce.json();
+    console.log(text.deck_id);
 
-    textHTML.innerHTML = text;
+    let cards = await fetch(`https://deckofcardsapi.com/api/deck/${text.deck_id}/draw/?count=2`);
+
+    let cardsJSON = await cards.json();
+
+    console.log(cardsJSON.cards);
+
+    playerImg.innerHTML = `<img src="${cardsJSON.cards[0].image}">`;
+    aiImg.innerHTML = `<img src="${cardsJSON.cards[1].image}">`;
+
+    if (parseFloat(cardsJSON.cards[0].value) > parseFloat(cardsJSON.cards[1].value)) {
+        resultDiv.innerHTML = "<h1>Вы выиграли</h1>";
+    }
+    else if (cardsJSON.cards[0].value == cardsJSON.cards[1].value) {
+        resultDiv.innerHTML = "<h1>Ничья</h1>";
+    }
+    else {
+        resultDiv.innerHTML = "<h1>Вы проиграли</h1>";
+    }
+
+    return text;
 }
