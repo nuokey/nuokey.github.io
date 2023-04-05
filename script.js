ff79253a8d7a9e78ee4f717146aac8f90eaa7c97
Timo <66961498+nuokey@users.noreply.github.com>
2023-04-05 09:03:24 +0300

    Update script.js

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ let computerPoints = 0;
 
 let computerHTML = '';
 
+takeCard();
+takeCard();
+
 async function takeCard() {
     let responce = await fetch(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`);
 
@@ -49,7 +52,12 @@ async function takeCard() {
             computerPoints += 10;
         }
         else if (cardsJSON.cards[1].value == "ACE") {
-            computerPoints += 1;
+            if (computerPoints + 11 > 21) {
+                computerPoints += 1
+            }
+            else {
+                computerPoints += 11;
+            }
         }
         else {
             computerPoints += parseFloat(cardsJSON.cards[1].value);
@@ -58,7 +66,36 @@ async function takeCard() {
     }
 }
 
-function stopTaking() {
+async function stopTaking() {
+    while (computerPoints < 18) {
+        let responce = await fetch(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`);
+
+        let text = await responce.json();
+        console.log(text.deck_id);
+
+        let cards = await fetch(`https://deckofcardsapi.com/api/deck/${text.deck_id}/draw/?count=2`);
+
+        let cardsJSON = await cards.json();
+
+        console.log(cardsJSON.cards);
+
+        if (cardsJSON.cards[1].value == "QUEEN" || cardsJSON.cards[1].value == "KING" || cardsJSON.cards[1].value == "JACK") {
+            computerPoints += 10;
+        }
+        else if (cardsJSON.cards[1].value == "ACE") {
+            if (computerPoints + 11 > 21) {
+                computerPoints += 1
+            }
+            else {
+                computerPoints += 11;
+            }
+        }
+        else {
+            computerPoints += parseFloat(cardsJSON.cards[1].value);
+        }
+        computerHTML += `<img src="${cardsJSON.cards[1].image}">`;
+    }
+
     computerImgsHTML.innerHTML = computerHTML;
     if (computerPoints > 21 && playerPoints > 21) {
         resultHTML.innerHTML = "Ничья";
@@ -78,36 +115,4 @@ function stopTaking() {
     else {
         resultHTML.innerHTML = "Ничья";
     }
-}
-
-
-async function requestCards() {
-    let responce = await fetch(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`);
-
-    let text = await responce.json();
-    console.log(text.deck_id);
-
-    let cards = await fetch(`https://deckofcardsapi.com/api/deck/${text.deck_id}/draw/?count=2`);
-
-    let cardsJSON = await cards.json();
-
-    console.log(cardsJSON.cards);
-
-    playerImg.innerHTML = `<img src="${cardsJSON.cards[0].image}">`;
-
-    
-
-
-
-    // if (parseFloat(cardsJSON.cards[0].value) > parseFloat(cardsJSON.cards[1].value)) {
-    //     resultDiv.innerHTML = "<h1>Вы выиграли</h1>";
-    // }
-    // else if (cardsJSON.cards[0].value == cardsJSON.cards[1].value) {
-    //     resultDiv.innerHTML = "<h1>Ничья</h1>";
-    // }
-    // else {
-    //     resultDiv.innerHTML = "<h1>Вы проиграли</h1>";
-    // }
-
-    return text;
 }
